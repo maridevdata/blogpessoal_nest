b01fe76fbfcef0a7640094b10e5a2034d931c763
maridevdata <marianaluisa.works@gmail.com>
2025-08-07 23:28:44 -0300

    Camada de Seg. Parte 01 e 02

---
+++ src/auth/constants/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'T2B0EFDEcaOwk6v6aKHBBWIe7AyVepN9y2ezFqulLZYyafS9iVHfHABDqqRCnB9I',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact("Mariana Luisa","https://github.com/maridevdata","marianaluisa.works@gmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   process.env.TZ = '-3:00'
 
